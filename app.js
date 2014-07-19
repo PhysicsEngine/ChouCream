ce1c0375d0d549bb282d724a6e9097255432df95
Lewuathe <lewuathe@me.com>
2014-07-19 17:36:49 +0900

    Create app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var express = require('express');
+// web.js
+var express = require("express");
+var logfmt = require("logfmt");
 var app = express();
 
-app.get('/hello.txt', function(req, res){
-  res.send('Hello World');
-});
+app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-    res.send("This is ChouCream");
+  res.send('This is ChouCream');
 });
 
-var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
