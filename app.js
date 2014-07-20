bc436b7e3c6d7ba9f73d2a1d30d0553f62405ae1
tishibas67 <t_ishibas67@yahoo.co.jp>
2014-07-20 15:38:53 +0900

    connect ECT with Express

--- app.js
+++ app.js
@@ -2,11 +2,20 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var ECT = require('ect');
+
+//ECT env
+app.engine('ect', ECT({ watch: true, root: __dirname + '/views', ext: '.ect' }).render);
+app.set('view engine', 'ect');
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-    res.send('This is ChouCream');
+    res.send('This is ChouCream and supervisor test');
+});
+app.get('/ect', function(req,res){ res.render('index',{ 
+    title: 'ect test title'
+    });
 });
 
 var port = Number(process.env.PORT || 5000);
