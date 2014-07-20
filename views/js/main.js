77e6a669a734c975a99db59ce98de21cfa0a3521
tishibas67 <t_ishibas67@yahoo.co.jp>
2014-07-21 00:52:24 +0900

    receive msg using socket.io

--- views/js/main.js
+++ views/js/main.js
@@ -1,4 +1,23 @@
 $(function() {
-    $.ajax("service/post",
-        );
+    var socket = io();
+    $('#content-post').submit(function(){
+        socket.emit('post', $('#content').val());
+        $('#content').val('');
+        return false;
+    });
+
+    socket.on('post', function(msg){
+        //$('#messages').append($('<li>').text(msg));
+    });
+
+
+
+    //$.on('submit_btn', function(){
+    //        $.ajax({
+    //            url: "service/post",
+    //            type: "POST",
+    //            username: ""
+    //        });
+    //    }
+    //)
 });
