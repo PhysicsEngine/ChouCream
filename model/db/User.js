37abd45c42b7781fc1be57d5a0ad33fd1cbab058
Lewuathe <lewuathe@me.com>
2014-07-20 09:28:55 +0900

    Fetch API

--- model/db/User.js
+++ model/db/User.js
@@ -2,23 +2,100 @@ var MongoClient = require('mongodb').MongoClient;
 var format = require('util').format;
 var MONGOHQ_URL = process.env['MONGOHQ_URL'];
 
-function User(username, token, icon_url, callback) {
-    var self = this;
-    MongoClient.connect(MONGOHQ_URL, function(err, db) {
-        console.log('MongoClient.connect');
-        if(err) throw err;
 
-        var collection = db.collection('user');
-        collection.insert({username: username, token: token, icon_url: icon_url}, function(err, docs) {
-            console.log("insert");
-            if (err) throw err;
-            db.close();
-            callback(err, docs);
+/**
+ *  This model represents user account 
+ *
+ *  @param username
+ *  @param token
+ *  @param icon_url 
+ *  @param callback
+ */
+function User(username) {
+    var self = this;
+    self.name = username;
+}
+/**
+ *  Fetch data from MongoHQ instance
+ *
+ *  @param callback
+ */
+User.prototype.fetch = function(callback) {
+    var self = this;
+    function selectAction(collection, closeCb) {
+        collection.find({username:self.name}).toArray(function(err, results) {
+            closeCb(err, results);
         });
+    }
+
+    self.connect(selectAction, function(err, results) {
+        if (! results.length == 1) {
+            console.log("There is mode than one record");
+            throw err;
+        }
+        self.token = results[0].token;
+        self.icon_url = results[0].icon_url;
+        callback(err, results[1]);
     });
 }
 
+/**
+ *  Create user object
+ *
+ *  @param callback
+ */
+User.prototype.save = function(callback) {
+    var self = this;
+    var username = self.name;
+    var token = self.token;
+    var icon_url = self.icon_url;
+    function insertAction(collection, closeCb) {
+        collection.insert({username: username, token: token, icon_url: icon_url}, function(err, results) {
+            // Closing db resource
+            closeCb(err, results);
+        });
+    }
 
+    self.connect(insertAction, callback);
+}
+
+/**
+ *  Remove instance method
+ *
+ *  @param username
+ *  @param callback
+ */
+User.prototype.remove = function(callback) {
+    var self = this;
+    var username = self.name;
+    function removeAction(collection, closeCb) {
+        collection.remove({username: username}, function(err, results) {
+            closeCb(err, results);
+        });
+    }
+
+    self.connect(removeAction, callback);
+}
+
+
+/**
+ *  Instance  method for creating connection 
+ *
+ *  @param action
+ *  @param callback
+ */
+User.prototype.connect = function(action, callback) {
+    MongoClient.connect(MONGOHQ_URL, function(err, db) {
+        if(err) throw err;
+        var collection = db.collection('user');
+
+        // Do action for example CRUD
+        action(collection, function(err, results) {
+            db.close();
+            callback(err, results);
+        });
+    });
+}
 
 
 module.exports = User;
--- spec/User.spec.js
+++ spec/User.spec.js
@@ -1,9 +1,35 @@
 var User = require("../model/db/User.js");
 
 describe("User model test", function() {
-    it("should access MongoHQ instance", function(done) {
-        var user = new User("kaisasak", "123456789", "http://example.com", function(err, docs) {
-            expect(docs).not.toBe(null);
+    it("should create user model instance", function() {
+        var user = new User("kaisasak");
+        expect(user.name).toBe("kaisasak");
+    });
+
+    it("should create into MongoHQ instance", function(done) {
+        var user = new User("kaisasak");
+        user.token = "123456789";
+        user.icon_url = "http://example.com";
+        user.save(function(err, results) {
+            done();
+        });
+    });
+
+    /*
+    it("should remove from MongoHQ instance", function(done) {
+        var user = new User("kaisasak");
+        user.remove(function(err, results) {
+            console.log(results);
+            done();
+        });
+    });
+    */
+
+    it("should fetch user model from MongoHQ instance", function(done) {
+        var user = new User("kaisasak");
+        
+        user.fetch(function(err, results) {
+            expect(user.token).toBe("123456789");
             done();
         });
     });
