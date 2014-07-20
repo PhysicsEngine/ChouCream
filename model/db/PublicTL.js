2e0e17eb31cfad645431d03fb6d877f6f2984ad5
Lewuathe <lewuathe@me.com>
2014-07-20 11:20:42 +0900

    Write somecomments

--- model/db/PublicTL.js
+++ model/db/PublicTL.js
@@ -3,10 +3,20 @@ var format = require('util').format;
 var MONGOHQ_URL = process.env['MONGOHQ_URL'];
 var ModelUtil = require('./ModelUtil.js');
 
+/**
+ *  This model represents all public timeline
+ *  posted by each users
+ *
+ */
 function PublicTL() {
     var self = this;
 }
 
+/**
+ *  Get public timeline
+ *
+ *  @param limit
+ */
 PublicTL.prototype.fetch = function(limit, callback) {
     var self = this;
     function selectAction(collection, closeCb) {
