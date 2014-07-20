c5596a81a7839e854aff1ec7ce4546cbabb218e4
tishibas67 <t_ishibas67@yahoo.co.jp>
2014-07-21 03:22:21 +0900

    fix web_url

--- libs/TwitterPassport.js
+++ libs/TwitterPassport.js
@@ -4,7 +4,7 @@ exports.initialize = function(config) {
     return new TwitterStrategy({
             consumerKey: config.service.Twitter.CONSUME_KEY,
             consumerSecret: config.service.Twitter.CONSUME_SECRET,
-            callbackURL: choucreamConfig.WEB_URL + config.service.Twitter.callbackURL
+            callbackURL: process.env.WEB_URL + config.service.Twitter.callbackURL
         },
         function(token, tokenSecret, profile, done) {
             user = {
