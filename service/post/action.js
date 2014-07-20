32884c665bca5dfb07e9824961ca0de41bb8911e
tishibas67 <t_ishibas67@yahoo.co.jp>
2014-07-20 20:28:24 +0900

    merge app

--- service/post/action.js
+++ service/post/action.js
@@ -1,7 +1,8 @@
 var User = require("../../models/db/User.js");
 
 function action(req, res){
-    var user = new User(req.user.name);
+    //var user = new User(req.user.name);
+    var user = new User('kaisasak');
     user.post(req.body.content);
 }
 
