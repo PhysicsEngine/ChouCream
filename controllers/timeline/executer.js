fff422c03b6b7df5fcd623cb0c1078c82d8aed34
shoe116 <shupoppo.116@gmail.com>
2014-07-20 17:09:56 +0900

    add timeline executer

---
+++ controllers/timeline/executer.js
@@ -0,0 +1,14 @@
+var PublicTL = require("../../models/db/PublicTL.js");
+var User = require("../../models/db/User.js");
+
+function execute(req, res){
+    var publicTl = new PublicTL();
+    publicTL.fetch(10, function(error, timeline){
+        if(error){
+            res.send("ChouCream has some unko cream"); 
+        }
+        res.render('timeline', {timeline:timeline});
+    });
+}
+
+exports.execute = execute;
