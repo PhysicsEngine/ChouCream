5ad04d27ca8ca789bc1201bd0959926e97a3cea8
tishibas67 <t_ishibas67@yahoo.co.jp>
2014-07-20 23:39:53 +0900

    start making the part of ajax

--- service/post/action.js
+++ service/post/action.js
@@ -1,8 +1,7 @@
 var User = require("../../models/db/User.js");
 
 function action(req, res){
-    //var user = new User(req.user.name);
-    var user = new User('kaisasak');
+    var user = new User(req.user.name);
     user.post(req.body.content);
 }
 
